0cbf9e053cf4cc146446d25d902375cf8dc829e0
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-27 12:59:16 +0545

    hover effect

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,13 @@ const Header = () => {
   return (
     <header className="p-5 text-white flex gap-2  ">
       <lord-icon
+        className="cursor-pointer"
         src="https://cdn.lordicon.com/hrrhosyf.json"
         trigger="hover"
         stroke="bold"
         colors="primary:#ffffff,secondary:#e4e4e4"
       ></lord-icon>
-      <h1 className="text-2xl md:text-3xl  w-fit font-bold font-[Poppins]">
+      <h1 className="text-2xl md:text-3xl cursor-pointer  w-fit font-bold font-[Poppins]">
         Password Manager
       </h1>
     </header>
