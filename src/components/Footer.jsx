3874a404f1ffed7b1a0db06799785a69a5813b5f
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-27 10:38:15 +0545

    make footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 text-white w-full py-2">
+      <p className="text-center ">
+        <a href="https://lordicon.com/">Animated icons by Lordicon.com</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
