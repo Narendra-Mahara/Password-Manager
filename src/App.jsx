7613a7b067f356d15427766e36fbbd1f616ef6fb
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-27 10:41:07 +0545

    add  gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="text-center">Hello</div>;
+  return (
+    <div className="h-svh">
+      <div className="absolute top-0 z-[-2] h-svh w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_50%_at_20%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] md:bg-[radial-gradient(ellipse_30%_50%_at_10%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <Header />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
