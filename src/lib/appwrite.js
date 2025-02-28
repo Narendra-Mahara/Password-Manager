ca9e07051abfcb298a0e4dcb0299638246371ab8
Narendra Mahara <narendramahara85131@gmail.com>
2025-02-28 13:54:08 +0545

    export client

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,6 +1,6 @@
 import { Client, Databases } from "appwrite";
 
-const client = new Client()
+export const client = new Client()
   .setEndpoint("https://cloud.appwrite.io/v1")
   .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
