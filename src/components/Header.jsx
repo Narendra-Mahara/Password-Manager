9b71c8ad6ac479549038a57b491577a3dcdda358
Narendra Mahara <narendramahara9868@gmail.com>
2025-03-12 16:32:53 +0545

    add link to homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,19 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="p-5 text-white flex gap-2  ">
-      <lord-icon
-        className="cursor-pointer"
-        src="https://cdn.lordicon.com/hrrhosyf.json"
-        trigger="hover"
-        stroke="bold"
-        colors="primary:#ffffff,secondary:#e4e4e4"
-      ></lord-icon>
-      <h1 className="text-2xl md:text-3xl cursor-pointer  w-fit font-bold font-[Poppins]">
-        Password Manager
-      </h1>
+    <header className="p-5">
+      <a href="/" className=" flex w-fit text-white gap-2  ">
+        <lord-icon
+          className="cursor-pointer"
+          src="https://cdn.lordicon.com/hrrhosyf.json"
+          trigger="hover"
+          stroke="bold"
+          colors="primary:#ffffff,secondary:#e4e4e4"
+        ></lord-icon>
+        <h1 className="text-2xl md:text-3xl cursor-pointer  w-fit font-bold font-[Poppins]">
+          Password Manager
+        </h1>
+      </a>
     </header>
   );
 };
